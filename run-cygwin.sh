8cc0e674c2cbea4e30ea6aa15b9b44a2f2f1c192
waldi <waldi>
2002-04-30 14:33:07 +0000

    shouldn't longer needed

--- run-cygwin.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# run this once to get tuxbox cdk running under cygwin
-# (p) +tbn 2002
-# 
-cd ../boot && patch -p1 < ../cdk/Patches/cygwin/ppcboot.diff
-cd ../cdk && cp Patches/cygwin/mkimage.exe ../boot/ppcboot/tools/
