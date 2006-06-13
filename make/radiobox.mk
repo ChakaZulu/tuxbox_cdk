ec5b1dd57da18862624b5a7af3a374623ca55e94
barf <barf>
2006-06-13 15:28:54 +0000

    No radiobox.mk in HEAD

--- make/radiobox.mk
+++
@@ -1,7 +0,0 @@
-# tuxbox/radiobox
-
-$(appsdir)/tuxbox/radiobox/config.status: bootstrap libboost libcurl libfreetype libmad libid3tag libvorbisidec libtuxbox misc_libs tuxbox_libs misc_tools lufs
-	cd $(appsdir)/tuxbox/radiobox && $(CONFIGURE)
-
-radiobox: $(appsdir)/tuxbox/radiobox/config.status
-	$(MAKE) -C $(appsdir)/tuxbox/radiobox all install
