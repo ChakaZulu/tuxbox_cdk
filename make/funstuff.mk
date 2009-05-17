a062746c9ce772e831c97dc03ea7db4e7dfa9bbc
rhabarber1848 <rhabarber1848>
2009-05-17 00:25:48 +0000

    create flash targets for lcd savers programs, like flash-aniplay, these targets exist for apps/tuxbox/funstuff/lcd-savers/*.c

--- make/funstuff.mk
+++ make/funstuff.mk
@@ -6,3 +6,15 @@ $(appsdir)/tuxbox/funstuff/config.status: bootstrap
 $(DEPDIR)/funstuff: $(appsdir)/tuxbox/funstuff/config.status
 	$(MAKE) -C $(appsdir)/tuxbox/funstuff all install
 	touch $@
+
+if TARGETRULESET_FLASH
+
+lcdsaversfiles = $(basename $(notdir $(wildcard $(appsdir)/tuxbox/funstuff/lcd-savers/*.c)))
+
+$(patsubst %,flash-%,$(lcdsaversfiles)):
+	@make $(flashprefix)/root/bin/$(patsubst flash-%,%,$@)
+
+$(patsubst %,$(flashprefix)/root/bin/%,$(lcdsaversfiles)): funstuff | $(flashprefix)/root
+	$(INSTALL) -m 755 $(targetprefix)/bin/$(notdir $@) $@
+
+endif
