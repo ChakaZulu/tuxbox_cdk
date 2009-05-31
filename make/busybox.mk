9f644b1450e2be8bb868e3e246abeb1213a13412
rhabarber1848 <rhabarber1848>
2009-05-31 15:32:44 +0000

    clean up busybox m4 stuff, use all boxtypes

--- make/busybox.mk
+++ make/busybox.mk
@@ -1,37 +1,38 @@
+
+BUSYBOX_M4 = -D$(BOXTYPE)
+BUSYBOX_M4 += -Dcustomizationsdir=$(customizationsdir)
+
 # the following if-statements look silly, but:
 # http://www.gnu.org/software/automake/manual/html_node/Conditionals.html#Conditionals
 # "You may only test a single variable in an if statement"
 if ENABLE_IDE
-BB_IDE=-Dide
+BUSYBOX_M4 += -Dide
 endif
 if ENABLE_MMC
-BB_IDE=-Dide
+BUSYBOX_M4 += -Dide
 endif
 
 if ENABLE_EXT3
-BB_EXT3=-Dextfs
+BUSYBOX_M4 += -Dextfs
 endif
 if ENABLE_EXT2
-BB_EXT3=-Dextfs
+BUSYBOX_M4 += -Dextfs
 endif
 
 if KERNEL26
-BB_KERNEL26=-Dkernel26
+BUSYBOX_M4 += -Dkernel26
 endif
 if ENABLE_FS_CIFS
-BB_CIFS=-Dcifs
+BUSYBOX_M4 += -Dcifs
 endif
 # this option is not used for yadd builds, they need nfs for booting
 if ENABLE_FS_NFS
-BB_NFS=-Dnfs
-endif
-if BOXTYPE_DREAMBOX
-BB_BOXTYPE=-Ddreambox
+BUSYBOX_M4 += -Dnfs
 endif
 
 $(DEPDIR)/busybox: bootstrap @DEPENDS_busybox@ $(busybox_conf) Patches/busybox.diff
 	@PREPARE_busybox@
-	m4 -Dyadd $(BB_BOXTYPE) $(BB_IDE) $(BB_EXT3) $(BB_KERNEL26) $(BB_CIFS) $(BB_NFS) -DPREFIX="\"$(targetprefix)\"" -Dcustomizationsdir=$(customizationsdir) $(busybox_conf) > @DIR_busybox@/.config
+	m4 $(BUSYBOX_M4) -Dyadd -DPREFIX="\"$(targetprefix)\"" $(busybox_conf) > @DIR_busybox@/.config
 	cd @DIR_busybox@ && \
 		$(MAKE) all install \
 			CROSS_COMPILE=$(target)- \
@@ -44,7 +45,7 @@ if TARGETRULESET_FLASH
 
 flash-busybox: bootstrap $(flashprefix)/root @DEPENDS_busybox@ $(busybox_conf) Patches/busybox.diff
 	@PREPARE_busybox@
-	m4 -Dflash $(BB_BOXTYPE) $(BB_IDE) $(BB_EXT3) $(BB_KERNEL26) $(BB_CIFS) $(BB_NFS) -DPREFIX="\"$(flashprefix)/root\"" -Dcustomizationsdir=$(customizationsdir) $(busybox_conf) > @DIR_busybox@/.config
+	m4 $(BUSYBOX_M4) -Dflash -DPREFIX="\"$(flashprefix)/root\"" $(busybox_conf) > @DIR_busybox@/.config
 	cd @DIR_busybox@ && \
 		$(MAKE) all install \
 			CROSS_COMPILE=$(target)- \
