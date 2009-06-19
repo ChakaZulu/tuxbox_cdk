ebc163d70dd3e9ee3242537507cf2286140bd2fa
rhabarber1848 <rhabarber1848>
2009-06-19 21:10:18 +0000

    merge dosfstools.mk with ide-apps.mk

--- make/ide-apps.mk
+++ make/ide-apps.mk
@@ -4,7 +4,7 @@
 #
 
 
-ide_apps: hdparm utillinux e2fsprogs parted hddtemp xfsprogs smartmontools
+ide_apps: hdparm utillinux e2fsprogs parted hddtemp xfsprogs smartmontools @DOSFSTOOLS@
 
 IDE_DEPSCLEANUP = rm -f .deps/hdparm .deps/e2fsprogs .deps/utillinux .deps/parted .deps/hddtemp .deps/xfsprogs .deps/smartmontools
 
@@ -440,6 +440,31 @@ $(flashprefix)/root/sbin/smartctl: bootstrap @DEPENDS_smartmontools@ | $(flashpr
 	@FLASHROOTDIR_MODIFIED@
 
 endif
+
+$(DEPDIR)/dosfstools: bootstrap @DEPENDS_dosfstools@
+	@PREPARE_dosfstools@
+	cd @DIR_dosfstools@ && \
+		$(MAKE) all \
+		CC=$(target)-gcc \
+		OPTFLAGS="$(TARGET_CFLAGS) -fomit-frame-pointer -D_FILE_OFFSET_BITS=64" && \
+		@INSTALL_dosfstools@		
+		@CLEANUP_dosfstools@
+	touch $@
+
+if TARGETRULESET_FLASH
+
+flash-dosfstools: $(flashprefix)/root/sbin/mkfs.msdos
+
+$(flashprefix)/root/sbin/mkfs.msdos: dosfstools | $(flashprefix)/root
+	@$(INSTALL) -d $(flashprefix)/root/sbin
+	@for i in mkdosfs dosfsck; do \
+	$(INSTALL) $(targetprefix)/sbin/$$i $(flashprefix)/root/sbin; done;
+		@ln -sf mkdosfs $@
+		@ln -sf mkdosfs $(flashprefix)/root/sbin/mkfs.vfat
+		@ln -sf dosfsck $(flashprefix)/root/sbin/fsck.msdos
+		@ln -sf dosfsck $(flashprefix)/root/sbin/fsck.vfat
+	@FLASHROOTDIR_MODIFIED@
+
 endif
 
-
+endif
