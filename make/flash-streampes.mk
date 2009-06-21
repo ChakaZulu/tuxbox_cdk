dbbb6146b550a7b7f607917045c027207e081311
dbluelle <dbluelle>
2009-06-21 13:23:30 +0000

    Remove dependency to eventserver-lib for Dreambox

--- make/flash-streampes.mk
+++ make/flash-streampes.mk
@@ -5,7 +5,11 @@ flash-streampes: $(appsdir)/dvb/tools/stream/streampes $(appsdir)/dvb/zapit/src/
 	$(INSTALL) $(appsdir)/dvb/tools/stream/streamsec $(flashprefix)/root/sbin
 	$(INSTALL) $(appsdir)/dvb/tools/stream/streamts $(flashprefix)/root/sbin
 	$(INSTALL) $(appsdir)/dvb/tools/stream/streampes $(flashprefix)/root/sbin
+if BOXTYPE_DREAMBOX
+	$(INSTALL) $(appsdir)/dvb/tools/stream/udpstreampes $(flashprefix)/root/sbin
+else
 	$(INSTALL) $(appsdir)/dvb/tools/stream/udpstreamts $(flashprefix)/root/sbin
 	$(INSTALL) $(appsdir)/dvb/zapit/src/.libs/udpstreampes $(flashprefix)/root/sbin
+endif
 	@FLASHROOTDIR_MODIFIED@
 	@TUXBOX_CUSTOMIZE@
