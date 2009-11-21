e567da68413e5a2fa91a440737d523cf778648b6
rhabarber1848 <rhabarber1848>
2009-11-21 15:37:58 +0000

    Dbox2: Replace monolithic init scripts with modular ones, thanks to seife and testers: http://www.dbox2-tuning.net/forum/viewtopic.php?f=7&t=48824

--- make/etc.mk
+++ make/etc.mk
@@ -1,6 +1,6 @@
 yadd-etc: $(targetprefix)/etc/init.d/rcS
 
-$(targetprefix)/etc/init.d/rcS: root/etc/init.d/rcS.m4
+$(targetprefix)/etc/init.d/rcS:
 	$(MAKE) -C root install
 if ENABLE_IDE
 	echo $(HDD_MOUNT_ENTRY)	>> $(targetprefix)/etc/fstab
